984ccb94d14846191db85df65fb460bd2f99eec4
mhazm <mhazmiee@gmail.com>
2021-02-28 18:47:12 +0700

    newcp

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -7599,7 +7599,7 @@ class Adventure(commands.Cog):
                 log.exception("Error with the new character sheet", exc_info=exc)
                 continue
             userxp = int(xp + (xp * 0.5 * c.rebirths) + max((xp * 0.1 * min(250, c._int / 10)), 0))
-            usercp = int(cp + max((cp * 2 * min(1000, (c._luck + c._att) / 10)), 0))
+            usercp = int(cp + max((cp * 5 * min(1000, (c._luck + c._att) / 10)), 0))
             userxp = int(userxp * (c.gear_set_bonus.get("xpmult", 1) + daymult))
             usercp = int(usercp * (c.gear_set_bonus.get("cpmult", 1) + daymult))
             newxp += userxp
