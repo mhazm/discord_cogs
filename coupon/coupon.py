241871f6fe7b73456732cfa003b56ca83611a776
mhazm <mhazmiee@gmail.com>
2021-02-28 11:57:50 +0700

    kupon bahasa

--- coupon/coupon.py
+++ coupon/coupon.py
@@ -18,12 +18,12 @@ global_bank_check = commands.check(pred)
 
 
 class Coupon(commands.Cog):
-    """Creates redeemable coupons for credits.
+    """Membuat kupon untuk kredit.
 
-    The bank must be in guild mode and not global mode for this cog to work."""
+    Bank harus berada dalam guild mode, bukan global mode untuk mengaktifkan ini."""
 
     async def red_delete_data_for_user(self, **kwargs):
-        """Nothing to delete."""
+        """Tidak ada yang dapat dihapus."""
         return
 
     def __init__(self, bot):
@@ -38,24 +38,24 @@ class Coupon(commands.Cog):
     @commands.group()
     @global_bank_check
     async def coupon(self, ctx):
-        """Coupon commands."""
+        """Command Kupon."""
         pass
 
     @coupon.command(name="clearall")
     @checks.mod_or_permissions(manage_guild=True)
     @global_bank_check
     async def _clearall_coupon(self, ctx):
-        """Clears all unclaimed coupons."""
+        """Menghapus semua kupon yang belum di klaim."""
         await self.config.guild(ctx.guild).clear()
-        await ctx.send("All unclaimed coupon codes have been cleared from the list.")
+        await ctx.send("Semua kode kupon yang belum di klaim telah dihapus dari database.")
 
     @coupon.command(name="create")
     @checks.admin_or_permissions(manage_guild=True)
     @global_bank_check
     async def _create_coupon(self, ctx, credits: int):
-        """Generates a unique coupon code."""
+        """Membuat kode unik kupon"""
         if credits > 2 ** 63 - 1:
-            return await ctx.send("Try a smaller number.")
+            return await ctx.send("Coba gunakan nomer yang lebih kecil.")
         if credits < 0:
             return await ctx.send("Nice try.")
         code = str(uuid.uuid4())
@@ -64,20 +64,20 @@ class Coupon(commands.Cog):
             settings.update({code: credits})
             await self.config.guild(ctx.guild).coupons.set(settings)
             credits_name = await bank.get_currency_name(ctx.guild)
-            await ctx.author.send(f"I have created a coupon for `{humanize_number(credits)}` {credits_name}.\nThe code is: `{code}`")
+            await ctx.author.send(f"Kupon dibuat untuk `{humanize_number(credits)}` {credits_name}.\nKodenya adalah: `{code}`")
         except discord.Forbidden:
-            await ctx.send("I was not able to DM you the code. Please open your DMs and try again.")
+            await ctx.send("Kode tidak dapat dikirimkan melalui DM, karena kemungkinan kamu menolak semua DM.")
 
     @coupon.command(name="list")
     @checks.admin_or_permissions(manage_guild=True)
     @global_bank_check
     async def _list_coupon(self, ctx):
-        """Shows active coupon codes."""
+        """Melihat kode kupon yang aktif."""
         settings = await self.config.guild(ctx.guild).coupons()
         SPACE = "\N{SPACE}"
         msg = f"[Code]{SPACE * 30} | [Credits]\n"
         if len(settings) == 0:
-            msg += "No active codes."
+            msg += "Tidak ada kode yang aktif."
         else:
             for code, credits in settings.items():
                 msg += f"{code} | {humanize_number(credits)}\n"
@@ -87,7 +87,7 @@ class Coupon(commands.Cog):
     @coupon.command(name="redeem")
     @global_bank_check
     async def _redeem_coupon(self, ctx, coupon: str):
-        """Redeems a coupon code."""
+        """Klaim kupon."""
         if len(coupon) == 36:
             settings = await self.config.guild(ctx.guild).coupons()
             if coupon in settings:
@@ -98,11 +98,11 @@ class Coupon(commands.Cog):
                     extra = ""
                 except BalanceTooHigh as e:
                     await bank.set_balance(ctx.author, e.max_balance)
-                    extra = f"You've hit the economy cap of {humanize_number(e.max_balance)} {credits_name}, though."
+                    extra = f"Uang kamu sudah mencapai batas tertinggi ekonomi {humanize_number(e.max_balance)} {credits_name}."
                 del settings[coupon]
                 await self.config.guild(ctx.guild).coupons.set(settings)
-                await ctx.send(f"I have added {humanize_number(credits)} {credits_name} to your account. {extra}")
+                await ctx.send(f"Saya menambahkan {humanize_number(credits)} {credits_name} ke akun kamu. {extra}")
             else:
-                await ctx.send("This coupon either does not exist or has already been redeemed.")
+                await ctx.send("Kode kupon ini tidak tersedia atau mungkin telah di klaim oleh member lain.")
         else:
-            await ctx.send("This is not a valid coupon code.")
+            await ctx.send("Kode kupon kamu tidak valid.")
